a97512faa9a85b64b4e88f7347cb0973d4dbd208
Ryan Hallisey <rhallise@redhat.com>
2017-03-15 00:06:27 -0400

    Refactor the loggger again
    Because the log object was being instantiated in each call,
    the locations of the event was never being recored. Log
    instantiation needs to be done in a package.

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -6,29 +6,32 @@ import (
 	"os"
 )
 
-func Error(err string) {
-	errorHandle := io.Writer(os.Stderr)
+var Error *log.Logger
+var Warning *log.Logger
+var Info *log.Logger
 
+func init() {
+
+	errorHandle := io.Writer(os.Stderr)
 	Err := log.New(errorHandle,
 		"ERROR: ",
 		log.Ltime|log.Lshortfile)
-	Err.Println(err)
-}
 
-func Warning(err string) {
 	warningHandle := io.Writer(os.Stdout)
-
 	Warn := log.New(warningHandle,
 		"WARNING: ",
 		log.Lshortfile)
-	Warn.Println(err)
-}
 
-func Info(err string) {
 	infoHandle := io.Writer(os.Stdout)
-
 	Information := log.New(infoHandle,
 		"INFO: ",
 		log.Lshortfile)
-	Information.Println(err)
+
+	Error = Err
+	Warning = Warn
+	Info = Information
+}
+
+func NewLogger() (*log.Logger, *log.Logger, *log.Logger) {
+	return Error, Warning, Info
 }
